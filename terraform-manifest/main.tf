1c345af5536ff6c24242207b45d33c1ff13efaeb
alskill <138034489+alskill@users.noreply.github.com>
2025-07-02 15:39:01 +0530

    Delete terraform-manifest directory

--- terraform-manifest/main.tf
+++
@@ -1,6 +0,0 @@
-# The default provider configuration; resources that begin with `aws_` will use
-# it as the default, and it can be referenced as `aws`.
-provider "aws" {
-  region = "ap-south-1"
-}
-
